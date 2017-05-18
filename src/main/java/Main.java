59a081efa2b16be3aa15fc17b031f6f54c68a629
Keith Lockwood <kwlockwo@gmail.com>
2017-05-18 16:13:16 +1000

    logging to stdout

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,42 +1,37 @@
 import java.io.IOException;
+
 import javax.servlet.ServletException;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.*;
-import java.net.URI;
-import java.net.URISyntaxException;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
 public class Main extends HttpServlet {
-  enum HttpHeader {
-    X_FORWARDED_FOR("X-Forwarded-For");
 
-    private String key;
+	private static final Logger logger = LogManager.getLogger(Main.class);
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		logger.debug("Debug Message");
+		logger.error("Error Message");
+		logger.warn("Warn message");
+		logger.info("Info message");
+				
+		res.getWriter().print("Check your logs");
+	}
 
-    private HttpHeader(String key) {
-        this.key = key;
-    }
-
-    public String key() {
-        return this.key;
-    }
-  }
-
-  @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-      throws ServletException, IOException {
-    for (HttpHeader header : HttpHeader.values()) {
-      String v = req.getHeader(header.key);
-      resp.getWriter().print(header.key + ": " + v);
-    }
-  }
-
-  public static void main(String[] args) throws Exception{
-    Server server = new Server(Integer.valueOf(System.getenv("PORT")));
-    ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-    context.setContextPath("/");
-    server.setHandler(context);
-    context.addServlet(new ServletHolder(new Main()),"/*");
-    server.start();
-    server.join();
-  }
+	public static void main(String[] args) throws Exception {
+		Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+		context.setContextPath("/");
+		server.setHandler(context);
+		context.addServlet(new ServletHolder(new Main()), "/*");
+		server.start();
+		server.join();
+	}
 }
